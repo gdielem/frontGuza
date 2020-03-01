b377bb1863c6c51d0fb2f38df05987093dbede01
Dieleman Guillaume <guillaume.dieleman@hotmail.com>
2020-03-01 20:38:01 +0100

    Add cookies management + login page

--- index.js
+++ index.js
@@ -1,43 +1,79 @@
 const BACKENDURL = "http://192.168.1.17:8000/polls";
 
-let visibleElement =[];
+let visibleElements =[];
 //backArrow :
 const backArrow = $("#back");
+visibleElements.push(backArrow);
 
+//Connexion items:
+const connexion_menu = $("#connexionView");
+const button_connexion = $("#buttonConnexion");
+visibleElements.push(connexion_menu);
 
 //Client menu items:
-const client_menu = $("#menu_client");
+const client_menu = $("#menu_client");visibleElements.push(client_menu);
+const button_new_client = $("#buttonNewClient");
 
 //Main menu items :
 const menu = $("#menu_main");
-const menu_client = $("#client")
+const deconnexion = $("#logOut");
+const button_client_view = $("#client");
+visibleElements.push(menu);visibleElements.push(deconnexion);
 
-//init state == Main menu view :
-hide(client_menu);
-hide(backArrow);
+//check if the user is already connected and display the right interface :
+checkConnexionAndDisplay();
+
+
+
+//Gestion des événements :
+button_new_client.on("click",function () {
+    let nom = $("#newClientName");
+    makeRequest("POST",{action:"addClient",name:nom});
+});
+
+button_connexion.on("click",function () {
+   let email = $("#inputEmail").val();
+   let password = $("#inputPassword").val();
+   if(email =="g" && password == "g"){
+       hideEverythingsVisible();
+       show(menu);
+       show(deconnexion);
+       document.cookie = 'connected=true'; //Crée ou met à jour un cookie 'connected'
+       alert(document.cookie); //Affiche la liste
+   }
+});
+
+deconnexion.on("click",function () {
+    hideEverythingsVisible();
+    show(connexion_menu);
+    document.cookie = 'connected=false';
+    alert(document.cookie);
+});
 
 
 backArrow.on("click",function () {
-    for (let i = 0 ; i < visibleElement.length ;i++){
-        hide(visibleElement[i]);
-    }
+    hideEverythingsVisible();
     show(menu);
+    show(deconnexion);
 });
 
 
 
 
 
-menu_client.on("click",function () {
-    hide(menu);
+button_client_view.on("click",function () {
+    hideEverythingsVisible();
     show(backArrow);
     show(client_menu);
-    visibleElement.push(backArrow);
-    visibleElement.push(client_menu);
 
 });
 
 //functions :
+function checkConnexionAndDisplay() {
+    let connexionCookie = document.cookie;
+    console.log("cookie = " + connexionCookie);
+}
+
 function makeRequest(type,data){
     $.ajax({
         url:BACKENDURL,
@@ -61,4 +97,40 @@ function hide(a) {
 }
 function show(a) {
     a.css("display","block");
+    visibleElements.push(a);
+}
+function hideEverythingsVisible() {
+    for (let i = 0 ; i<visibleElements.length ; i++){
+        visibleElements[i].css("display","none");
+    }
+}
+function checkConnexionAndDisplay() {
+    console.log("cookie =" +document.cookie);
+    if (getCookie("connected")==="true"){
+        //init state == Main menu view :
+        hide(client_menu);
+        hide(backArrow);
+        hide(connexion_menu);
+        visibleElements.push(menu); visibleElements.push(deconnexion);
+    }
+    else {
+
+        hideEverythingsVisible();
+        show(connexion_menu);
+    }
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
